fe0a25b90cebf188a525e9dcec0ec0cfae1ed773
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-05-23 20:33:06 -0600

    Commit Final

---
+++ Serializacion/Serializacion/Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacion.Models
+{
+
+    [Serializable]
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string identidad { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+
+
+        public override string ToString()
+        {
+            return $" {nombre} ";
+        }
+    }
+}
--- Serializacion/Serializacion/Program.cs
+++ Serializacion/Serializacion/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serializacion.Models;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+int opcion = 0;
+List<Persona> personas = new List<Persona>();
+
+try {
+    /*Abir y leer el archivo */
+    byte[] data = File.ReadAllBytes("personas.bin");
+    MemoryStream memory = new MemoryStream(data);
+    BinaryFormatter formater = new BinaryFormatter();
+    personas = (List<Persona>)formater.Deserialize(memory);
+    memory.Close();
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Sin archivo por cargar de Personas");
+
+}
+
+do {
+
+    Console.WriteLine("1. Crear Persona  \n 2. Salir ");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+           
+           Console.WriteLine("Escribir Nombre");
+           string nm  = Console.ReadLine();
+
+            Console.WriteLine("Escribir Fecha Nacimiento año");
+            int anio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribir Fecha Nacimiento mes");
+            int mes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribir Fecha Nacimiento dia");
+            int dia = int.Parse(Console.ReadLine());
+
+            DateTime f = new DateTime(anio, mes, dia);
+
+
+            Console.WriteLine("Escribir Identidad");
+            string id = Console.ReadLine();
+
+            Persona p = new Persona() { 
+                nombre = nm , 
+                fechaNacimiento = f,
+                identidad = id
+
+            };
+
+            personas.Add(p);
+
+
+
+            break;
+        default:
+            break;
+    }
+
+} while (opcion != 2);
+
+
+foreach (Persona p in personas) {
+
+    Console.WriteLine(p.ToString());
+
+}
+
+/*Escribir y cerrar el archivo*/
+
+BinaryFormatter formateador = new BinaryFormatter();
+MemoryStream memoria = new MemoryStream();
+
+formateador.Serialize(memoria, personas);
+
+byte[] datoSerializados = memoria.ToArray();
+
+memoria.Close();
+File.WriteAllBytes("personas.bin", datoSerializados);
